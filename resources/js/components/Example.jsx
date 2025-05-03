10aa02d405e7c59e35e50d293bc8257900715f61
Vikram <44178908+vikramkumavat@users.noreply.github.com>
2025-05-03 09:24:29 +0530

    unknown changes

---
+++ resources/js/components/Example.jsx
@@ -0,0 +1,44 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+
+// function Example() {
+//     return (
+//         <div className="container">
+//             <div className="row justify-content-center">
+//                 <div className="col-md-8">
+//                     <div className="card">
+//                         <div className="card-header">Example Component</div>
+
+//                         <div className="card-body">I'm an example component!</div>
+//                     </div>
+//                 </div>
+//             </div>
+//         </div>
+//     );
+// }
+
+// export default Example;
+
+// if (document.getElementById('example')) {
+//     const Index = ReactDOM.createRoot(document.getElementById("example"));
+
+//     Index.render(
+//         <React.StrictMode>
+//             <Example/>
+//         </React.StrictMode>
+//     )
+// }
+
+
+// Example.js
+import React from 'react';
+
+const Example = () => {
+    return (
+        <div>
+            <h1>Hello, React!</h1>
+        </div>
+    );
+}
+
+export default Example;
